f481abb64105cc3fd531ae339d0470a22c938a11
Saksham989 <75685036+Saksham989@users.noreply.github.com>
2022-02-26 12:04:03 +0530

    Update Quiz.js

--- js/Quiz.js
+++ js/Quiz.js
@@ -30,16 +30,31 @@ class Quiz {
 
   play(){
     //write code here to hide question elements
-
+question.hide();
     //write code to change the background color here
-
+background("white");
     //write code to show a heading for showing the result of Quiz
-
+text("Result of Quiz" ,340,40)
+    
     //call getContestantInfo( ) here
-
+    Contestant.getContestantInfo();
 
     //write condition to check if contestantInfor is not undefined
-
+if(allContestants!==undefined){
+  var displayAns=230
+text("Note- Contestant who answered correctly are highlighted in green colour", 130,230)
+  for(var plr in allContestants){
+  var correctAns="2"
+  if(correctAns==allContestants[plr].answer){
+  fill("green");
+  }
+    else{
+ fill("red")  
+    }
+    displayAns+=30
+    text(allContestants[plr].name + "-" + allContestants[plr].answer, 250,displayAns)
+  }
+}
     //write code to add a note here
 
     //write code to highlight contest who answered correctly
